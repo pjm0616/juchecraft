9ab397b407b23549a4f90a1011308d0635675819
pjm0616 <pjm0616@gmail.com>
2010-11-04 23:12:54 +0900

    fixed line counting script

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-find . ui tools libs/libmpqgrp/ libs/luacpp/ res_raw/ -maxdepth 1 -name '*.cpp' -o -name '*.c' -o -name '*.h'|xargs wc -l
+find . ui libs/jcimg libs/luacpp/ res_raw/ tools/libs/libmpqgrp/ tools/grp_conv/ \
+	 -maxdepth 1 -name '*.cpp' -o -name '*.c' -o -name '*.h'|xargs wc -l
 
