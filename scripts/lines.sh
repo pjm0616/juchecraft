dea82367774c020ab302affc54ce6d845d0edb52
pjm0616 <pjm0616@gmail.com>
2010-11-08 12:14:38 +0900

    fixed some missing files in line counting script

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find . ui libs/jcimg libs/luacpp/ res_raw/ tools/libs/libmpqgrp/ tools/grp_conv/ \
+find . ui ui/* libs/jcimg libs/luacpp/ res_raw/ tools/libs/libmpqgrp/ tools/grp_conv/ \
 	 -maxdepth 1 -name '*.cpp' -o -name '*.c' -o -name '*.h'|xargs wc -l
 
