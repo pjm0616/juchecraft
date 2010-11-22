87353d490b3f2d5ee38dd7da12f3bcf5ff68e809
pjm0616 <pjm0616@gmail.com>
2010-11-22 12:23:47 +0900

    oops. missed 3 files

---
+++ src/game/Exception.cpp
@@ -0,0 +1,23 @@
+
+#include <string>
+#include <sstream>
+
+#include "game/Exception.h"
+
+void SC::trigger_assertion_failure(const char *file, int line, const char *func, const char *expr, const char *comment)
+{
+	std::stringstream buf;
+	buf << "Assertion failed at " 
+		<< func << "()"
+		<< " ["
+			<< file << ":" << line
+		<< "] "
+		<< "\"" << expr << "\"";
+	if(comment && *comment)
+		buf << " - \"" << comment << "\"";
+	throw new SC::Exception(buf.str());
+}
+
+
+
+
---
+++ src/game/Target.cpp
@@ -0,0 +1,127 @@
+// Copyright (C) 2010 Park Jeongmin <pjm0616@gmail.com>
+// See LICENSE.txt for details
+
+#include "config.h"
+
+#include "smart_ptrs.h"
+#include <string>
+#include <list>
+#include <map>
+#include <vector>
+#include <deque>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+
+#ifdef DEBUG
+#include <stdio.h>
+#include <iostream>
+#endif
+
+#include "defs.h"
+#include "compat.h"
+#include "luacpp/luacpp.h"
+#include "game/Types.h"
+#include "game/Exception.h"
+#include "game/Coordinate.h"
+#include "game/Target.h"
+#include "game/ObjectIdList.h"
+#include "game/actions/UnitAction.h"
+#include "game/orders/UnitOrder.h"
+#include "game/Object.h"
+#include "game/ObjectList.h"
+
+using namespace SC;
+
+
+
+
+
+
+Target::Target()
+{
+	this->clear();
+}
+
+Target::Target(const Coordinate &coord)
+{
+	this->set(coord);
+}
+
+Target::Target(const ObjectPtr &obj)
+{
+	this->set(obj);
+}
+
+Target::Target(const Target &o)
+{
+	switch(o.getType())
+	{
+	case Type::None:
+		this->clear();
+		break;
+	case Type::Coordinate:
+		this->set(o.getCoordinate());
+		break;
+	case Type::Object:
+		this->set(o.getObject());
+		break;
+	default:
+		SCAssert(!"Cannot happen. asiera-x384kp39c,9eu");
+	}
+}
+
+Target::~Target()
+{
+	this->clear();
+}
+
+
+
+void Target::clear()
+{
+	this->m_type = Target::Type::None;
+	this->m_target.obj.reset();
+}
+void Target::set(const Coordinate &coord)
+{
+	this->clear();
+	this->m_type = Target::Type::Coordinate;
+	this->m_target.coord = coord;
+}
+void Target::set(const ObjectPtr &obj)
+{
+	this->clear();
+	this->m_type = Target::Type::Object;
+	this->m_target.obj = obj;
+}
+
+
+const Coordinate &Target::getCoordinate() const
+{
+	SCAssert(this->getType() == Type::Coordinate);
+	return this->m_target.coord;
+}
+
+const ObjectPtr &Target::getObject() const
+{
+	SCAssert(this->getType() == Type::Object);
+	return this->m_target.obj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
