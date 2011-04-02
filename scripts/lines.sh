281a1e24f26d9fe6fbbae664259d178634a169ed
pjm0616 <pjm0616@gmail.com>
2011-04-02 15:21:55 +0900

    fixed line counting script

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-srcs="`find ./ libs/jcimg/ libs/luacpp/ tools/libs/libmpqgrp/ tools/grp_conv/ -maxdepth 1 -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.lua'`"
+srcs="`find ./ tools/libs/libmpqgrp/ tools/grp_conv/ -maxdepth 1 -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.lua'`"
 srcs="$srcs `find src/ res_raw/ -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.lua'`"
 
 wc $srcs
